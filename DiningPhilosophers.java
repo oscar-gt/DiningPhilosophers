f1649cecc5102c98462196a4764e415b485f08f0
Oscar <oscar.garcia_telles@hotmail.com>
2016-08-03 15:41:57 -0700

    all tests pass

--- DiningPhilosophers.java
+++ DiningPhilosophers.java
@@ -9,7 +9,6 @@ import java.util.concurrent.locks.Condition;	// For Condition[] self
  */
 public class DiningPhilosophers {
     DiningState[] state;	// Can be either thinking, hungry, or eating
-    Condition[] self;		// Will either wait() or signal()
 
     // This will let all the tests run (and fail)
     // You'll want to remove it once you actually create an array :)
@@ -19,19 +18,17 @@ public class DiningPhilosophers {
     public DiningPhilosophers(int nPhilosophers) {
         nPhil =  nPhilosophers;
         state = new DiningState[numPhilosophers()];
-        self = new Condition[numPhilosophers()];
         for(int i = 0; i < numPhilosophers(); i++)
         {
         	// All philosophers thinking initially, 
         	// just chillin'
         	state[i] = DiningState.THINKING;
-        	// self = ...
-        }
-        
-        
+        }    
     }
    
     // -----------------	takeForks()		-----------------
+    // Changes state to HUNGRY and philosopher
+    // is given the opportunity to eat.
     public void takeForks(int i) {
         Main.TPrint( "TakeForksAS:   i=" + i);
         // Philosophers want to eat!
@@ -41,17 +38,21 @@ public class DiningPhilosophers {
         while(state[i] != DiningState.EATING)
         {
         	try {
-				//self[i].wait();
-        		wait();
+        		wait(); // If not allowed to eat, we wait
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} catch(IllegalMonitorStateException e) {
+				//e.printStackTrace();
 			}
         }
         
     }
 
     // ----------------		returnForks()	-----------------
+    // When a philosopher is done eating, his/her state
+    // returns to THINKING and then the neighbors are 
+    // given an opportunity to eat.
     public void returnForks(int i) {
         Main.TPrint( "returnForks:   i=" + i );
         state[i] = DiningState.THINKING;	// Done eating
@@ -64,6 +65,10 @@ public class DiningPhilosophers {
     }
     
     // ----------------		test()		------------------
+    // This method lets a philosopher eat by checking
+    // that both neighbors are not eating. This is 
+    // the only code that can chagen a philosopher's state
+    // to EATING.
     public synchronized void test(int i)
     {
     	int oneLess = numPhilosophers() - 1;
@@ -71,15 +76,14 @@ public class DiningPhilosophers {
     	int leftNeighbor = (i + 1) % numPhilosophers();
     	// Checking if left, right neighbors are NOT eating and also
     	// checking of current philosopher is hungry
-    	if( (state[rightNeighbor] != DiningState.EATING) && (state[i] == DiningState.HUNGRY)
+    	if( (state[rightNeighbor] != DiningState.EATING) 
+    			&& (state[i] == DiningState.HUNGRY)
     			&& (state[leftNeighbor] != DiningState.EATING) )
     	{
-    		// Eating 
+    		// Eating now
     		state[i] = DiningState.EATING;
-    		//self[i].signal();
     		try {
-				//self[i].wait();
-        		notifyAll();
+        		notifyAll(); // Wakes up all processes waiting
 			} catch (IllegalMonitorStateException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -88,11 +92,13 @@ public class DiningPhilosophers {
     }
 
     // ---------------		numPhilosophers()	------------
+    // Returns the number of philosophers
     public int numPhilosophers() {
         return nPhil;
     }
 
-    // ---------------		getDiningState9)	------------
+    // ---------------		getDiningState(int i)	------------
+    // Returns state of philosopher
     public DiningState getDiningState(int i) {
         return state[i];
     }
